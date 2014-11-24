2ba0188b6408424101136a17155590f9ddc51aa5
bauner <bauner@gmx.de>
2014-11-24 12:33:39 +0100

    added exynos4 ril

--- src/java/com/android/internal/telephony/RIL.java
+++ src/java/com/android/internal/telephony/RIL.java
@@ -109,6 +109,7 @@ class RILRequest {
     //***** Instance Variables
     int mSerial;
     int mRequest;
+    long creationTime;
     Message mResult;
     Parcel mParcel;
     RILRequest mNext;
@@ -140,6 +141,7 @@ class RILRequest {
 
         rr.mRequest = request;
         rr.mResult = result;
+	rr.creationTime = System.currentTimeMillis();
         rr.mParcel = Parcel.obtain();
 
         if (result != null && result.getTarget() == null) {
