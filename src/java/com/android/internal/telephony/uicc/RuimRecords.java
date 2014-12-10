8913e999fb460708e9ee99f835e7a97d1db473b7
bauner <bauner@gmx.de>
2014-12-10 14:43:53 +0100

    merge cm changes

--- src/java/com/android/internal/telephony/uicc/RuimRecords.java
+++ src/java/com/android/internal/telephony/uicc/RuimRecords.java
@@ -552,14 +552,10 @@ public final class RuimRecords extends IccRecords {
                 data = (byte[])ar.result;
 
                 if (ar.exception != null) {
-                    if(mFh.needsFakeIccid()) {
-                        mIccId = FAKE_ICCID;
-                    } else {
-                        break;
-                    }
-                } else {
-                    mIccId = IccUtils.bcdToString(data, 0, data.length);
+                    break;
                 }
+
+                mIccId = IccUtils.bcdToString(data, 0, data.length);
 
                 log("iccid: " + mIccId);
 
--- src/java/com/android/internal/telephony/uicc/SIMRecords.java
+++ src/java/com/android/internal/telephony/uicc/SIMRecords.java
@@ -852,19 +852,15 @@ public class SIMRecords extends IccRecords {
             case EVENT_GET_ICCID_DONE:
                 isRecordLoadResponse = true;
 
-
                 ar = (AsyncResult)msg.obj;
                 data = (byte[])ar.result;
 
                 if (ar.exception != null) {
-                    if(mFh.needsFakeIccid()) {
-                        mIccId = FAKE_ICCID;
-                    } else {
-                        break;
-                    }
-                } else {
-                    mIccId = IccUtils.bcdToString(data, 0, data.length);
+                    break;
                 }
+
+                mIccId = IccUtils.bcdToString(data, 0, data.length);
+
                 log("iccid: " + mIccId);
 
             break;
